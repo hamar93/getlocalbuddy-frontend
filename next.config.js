1c1e8505b4aa46b60a82f65c5b7859d1a14199af
hamar93 <hamargabor1993@gmail.com>
2025-07-09 09:45:55 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withNextIntl = require('next-intl/plugin')();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  }
+}
+
+module.exports = withNextIntl(nextConfig)
