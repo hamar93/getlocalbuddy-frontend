799ff2239156cad8715e5a8cb746ad9ed1fbd138
hamar93 <hamargabor1993@gmail.com>
2025-07-09 22:38:53 +0200

    Update page.tsx

--- app/buddy/[id]/page.tsx
+++ app/buddy/[id]/page.tsx
@@ -42,7 +42,7 @@ export default function BuddyProfilePage({ params }: { params: { id: string } })
       <Header />
       <main className="pt-20 pb-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <BuddyProfile buddy={mockBuddy} />
+           <BuddyProfile />
         </div>
       </main>
       <Footer />
